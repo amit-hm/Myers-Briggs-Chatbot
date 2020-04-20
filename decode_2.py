d6c260c512cf774d8f0425947837e54bb566189a
Amit Hulandageri <36067915+amithm92@users.noreply.github.com>
2020-04-19 19:49:24 -0600

    Update decode_2.py

--- decode_2.py
+++ decode_2.py
@@ -40,7 +40,8 @@ print()
 if __name__ == '__main__':
 	model = decode_model_2(args)
 	
-	AddresseeId = input("1: Introversion| 2: Extroversion| 3: Sensing| 4: Intuition| 5: Feeling| 6: Thinking| 7: Perceiving| 8: Judging\nChoose one of the options: ")
+	if model.params.SpeakerMode == True:
+		AddresseeId = input("1: Introversion| 2: Extroversion| 3: Sensing| 4: Intuition| 5: Feeling| 6: Thinking| 7: Perceiving| 8: Judging\nChoose one of the options: ")
 	print("\nEnter 'end' to exit")
 	while True:
 		line = input ("You: ")
